261ea2611d112239efa126efef1143f7a9eb7d88
Daniel Lechok <dclechok@gmail.com>
2019-09-10 20:26:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(sentence)
-    sentence.each do |word|
-      word.reverse
+	new_array = sentence.split
+    new_array.each do |word|
+      word.reverse!
     end
+		p new_array.join(" ")
 end
